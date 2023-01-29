75daa81469566b9feca01a6c11e7326d1aaf957a
mhanuz <46758023+mhanuz@users.noreply.github.com>
2023-01-29 12:06:59 +0600

    Update dropdown.directive.ts

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,11 +1,11 @@
 import { Directive, HostBinding, HostListener } from "@angular/core";
-
+// @Directive: declare custom directive
 @Directive({
     selector: '[appDropdownDirective]'
 })
 export class DropdownDirective {
-    @HostBinding('class.open') isOpon = false;
-    @HostListener('click') toggleOpen(){
+    @HostBinding('class.open') isOpon = false; // will bind the property to the host element
+    @HostListener('click') toggleOpen(){ // will listen to the event emitted by host element
         this.isOpon = !this.isOpon;
     }
 
