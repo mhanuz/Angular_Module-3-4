b9e63174c06c5b300fc8d6aa3a19120b10dde3b3
mhanuz <46758023+mhanuz@users.noreply.github.com>
2023-01-29 10:08:33 +0600

    Update shopping-list.component.ts

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,6 +6,7 @@ import { Ingredient } from '../shared/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  // ingredients  is a  class, class array element should be its obejects 
   ingredients: Ingredient[] = [
     new Ingredient ('Apples',5),
     new Ingredient ('Tomatoes',10)
